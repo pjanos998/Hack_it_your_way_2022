1f05bed07fa9c63a4ff6f76cb2b63ed4aeabf81c
Hegyi Gellért <p.janos998@gmail.com>
2022-11-01 21:34:29 +0100

    Flutter

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+//https://codelabs.developers.google.com/codelabs/google-maps-in-flutter#0
+//This is just the base of the code, the part done in python will be implemented here in the future
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(47.497913, 19.040236);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
